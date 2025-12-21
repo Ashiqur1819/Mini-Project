95505168b9f6faef5c5159d5cda39a65045fdb0a
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-21 09:48:28 +0600

    Create user model

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser")
 const express = require("express")
 const app = express()
 
@@ -8,6 +9,7 @@ app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "public")))
+app.use(cookieParser())
 
 
 app.get("/", (req, res) => {
