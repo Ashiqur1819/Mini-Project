0ddf1ad72873e60e4a41b8a504e8ed49a4835080
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-24 10:16:16 +0600

    Update multer.config page

--- app.js
+++ app.js
@@ -1,162 +1,148 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const path = require("path")
-const cookieParser = require("cookie-parser")
-const userModel = require("./models/user")
-const postModel = require("./models/post")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const upload  = require("./config/multer.config")
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const upload = require("./config/multer.config");
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "public")))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 // Middlewares
 
-const isLoggedIn  = (req, res, next) => {
-
-    if(req.cookies.token === ""){
-        res.redirect("/login")
-    }
-    else{
-        jwt.verify(req.cookies.token, 'Nusrat', function(err, decoded) {
-            if(err){
-                res.send("Invalid Token")
-            }else{
-                req.user = decoded
-                next()
-            }
-});
-    }
-}
-
+const isLoggedIn = (req, res, next) => {
+  if (req.cookies.token === "") {
+    res.redirect("/login");
+  } else {
+    jwt.verify(req.cookies.token, "Nusrat", function (err, decoded) {
+      if (err) {
+        res.send("Invalid Token");
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  }
+};
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
 app.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
+app.post("/register", async (req, res) => {
+  const { name, username, email, password, age } = req.body;
 
+  const user = await userModel.findOne({ email });
 
-app.post("/register", async(req, res) => {
+  if (user) return res.status(500).send("Already have an account.");
 
-    const {name, username, email, password, age} = req.body
-
-    const user = await userModel.findOne({email})
-
-    if(user) return res.status(500).send("Already have an account.")
-
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, async(err, hash) => {
-        await userModel.create({
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      await userModel.create({
         name,
         username,
         email,
-        password : hash,
-        age
-    })
+        password: hash,
+        age,
+      });
 
-    const token = jwt.sign({username, email}, "Nusrat")
-    res.cookie("token", token)
+      const token = jwt.sign({ username, email }, "Nusrat");
+      res.cookie("token", token);
 
-    res.redirect("/profile")
-
+      res.redirect("/profile");
     });
-    })
- 
-    
-})
-
-
-app.post("/login", async(req, res) => {
-
-    const {email, password} = req.body
-
-    const user = await userModel.findOne({email})
-
-    if(!user) return res.status(500).send("Something went wrong!")
-
-    bcrypt.compare(password, user.password, function(err, result) {
-    if(result){
-        const token = jwt.sign({username: user.username, email: user.email}, "Nusrat")
-    res.cookie("token", token)
-        res.redirect("/profile")
-    }
-    else
-        res.send("Something went wrong!")
+  });
 });
 
-    
-    
-})
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) return res.status(500).send("Something went wrong!");
+
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      const token = jwt.sign(
+        { username: user.username, email: user.email },
+        "Nusrat"
+      );
+      res.cookie("token", token);
+      res.redirect("/profile");
+    } else res.send("Something went wrong!");
+  });
+});
 
 app.get("/logout", (req, res) => {
-    res.cookie("token", "")
-    res.redirect("/login")
-})
-
+  res.cookie("token", "");
+  res.redirect("/login");
+});
 
 app.get("/profile", isLoggedIn, async (req, res) => {
+  const user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
 
-    const user = await userModel.findOne({email: req.user.email}).populate("posts")
+  res.render("profile", { user });
+});
 
-    res.render("profile", {user})
-})
+app.post("/post", isLoggedIn, async (req, res) => {
+  const user = await userModel.findOne({ email: req.user.email });
 
-app.post("/post", isLoggedIn, async(req, res) => {
-    const user = await userModel.findOne({email : req.user.email})
+  const { content } = req.body;
 
-    const {content} = req.body
+  const post = await postModel.create({
+    user: user._id,
+    content,
+  });
 
-    const post = await postModel.create({
-        user: user._id,
-        content
-    })
+  user.posts.push(post._id);
+  await user.save();
 
-    user.posts.push(post._id)
-    await user.save()
-    
-    res.redirect("/profile")
+  res.redirect("/profile");
+});
 
-})
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  const user = await userModel.findOne({ email: req.user.email });
 
-app.get("/like/:id", isLoggedIn, async(req, res) => {
+  const post = await postModel.findOne({ _id: req.params.id }).populate("user");
 
-    const user = await userModel.findOne({email: req.user.email})
+  if (post.likes.indexOf(user._id) === -1) {
+    post.likes.push(user._id);
+  } else {
+    post.likes.splice(post.likes.indexOf(user._id), 1);
+  }
 
-    const post = await postModel.findOne({_id : req.params.id}).populate("user")
-
-    if(post.likes.indexOf(user._id) === -1){
-            post.likes.push(user._id)
-    }else{
-        post.likes.splice(post.likes.indexOf(user._id), 1)
-    }
-
-    await post.save()
-    res.redirect("/profile")
-
-
-})
+  await post.save();
+  res.redirect("/profile");
+});
 
 app.get("/profile/upload", (req, res) => {
-    res.render("profileUpload")
-})
+  res.render("profileUpload");
+});
 
-app.post("/upload", upload.single('profilePic'), isLoggedIn, async(req, res) => {
-    const user = await userModel.findOne({email : req.user.email})
-    user.profilePic = req.file.filename
-    await user.save()
-    res.redirect("/profile")
-})
+app.post(
+  "/upload",
+  upload.single("profilePic"),
+  isLoggedIn,
+  async (req, res) => {
+    const user = await userModel.findOne({ email: req.user.email });
+    user.profilePic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+  }
+);
 
-
-app.listen(3000)
+app.listen(3000);
--- config/multer.config.js
+++ config/multer.config.js
@@ -1,20 +1,17 @@
-const multer = require("multer")
-const crypto = require("crypto")
-const path = require("path")
-
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(process.cwd(), "public/images/uploads"));
   },
   filename: function (req, file, cb) {
-
     crypto.randomBytes(12, (err, name) => {
-        const fileName = name.toString("hex")+path.extname(file.originalname)
-        cb(null, fileName)
-    })
-    
-  }
-})
+      const fileName = name.toString("hex") + path.extname(file.originalname);
+      cb(null, fileName);
+    });
+  },
+});
 
-module.exports = upload = multer({ storage: storage })
+module.exports = upload = multer({ storage: storage });
